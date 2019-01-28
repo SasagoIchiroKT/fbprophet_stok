78219e271bc3f350756cb58f3e029e4c9687f695
IchiroSasago <335ichiro@gmail.com>
2019-01-28 11:26:45 +0900

    指標追加（なお機能しない）
    指標追加（なお機能しない）

--- Prophet.py
+++ Prophet.py
@@ -13,33 +13,52 @@ from sklearn import svm
 import talib as ta
 
 from fbprophet import Prophet #時系列予測ライブラリ
+
 #Prophetのインストール
 #'pip install fbprophet' を実行 
 
 #使用するcsvデータはYahoo Finance より取得.
 # https://finance.yahoo.com/quote/2181.T/history?p=2181.T&.tsrc=fin-srch
 
+
+
+
 data = pd.DataFrame()
 file_name = sys.argv[1] #GUI.pyから得たカレントパスを取得する
-data2 = pd.read_csv(file_name, skiprows=1,header=None, names=['ds','Open','High','Low','Close','y','Volume',''])
+data2 = pd.read_csv(file_name, skiprows=1,header=None, names=['ds','Open','High','Low','Close','y','Volume'])
 print(type(data2['Close']))
 print(data2)
 data = data.append(data2)
 
+#指標追加
+def nfl_sunday(ds,y):
+    date = pd.to_datetime(ds)
+    end = pd.to_datetime(y)
+
+data['nfl_sunday'] = data['ds'].apply(nfl_sunday)
+
 #モデル作成
 model = Prophet()
+
+#指標の追加
+model.add_regressor('nfl_sunday')
+
 model.fit(data)
 
 #描画の設定,何日分出力する、など
 future_data = model.make_future_dataframe(periods=250, freq = 'd')
 future_data = future_data[future_data['ds'].dt.weekday < 5]
 
+future_data['nfl_sunday'] = future_data['ds'].apply(nfl_sunday)
+
 # 予測
 forecast_data = model.predict(future_data)
 
 # 描画
 fig = model.plot(forecast_data)
+
 model.plot_components(forecast_data)
+
 plt.legend()
 
 plt.show()
--- library_test/TA-Lib_test.py
+++ library_test/TA-Lib_test.py
@@ -11,8 +11,8 @@ import talib as ta
 ta.get_function_groups
 
 
-filename = 'TOYOTA.csv' # 日経平均株価データ
-df = pd.read_csv(filename,index_col='date', parse_dates=True)
+filename = '7203.csv' # 日経平均株価データ
+df = pd.read_csv(filename,index_col='Date', parse_dates=True)
 df.index = pd.to_datetime(df.index)
 
 print(df)
@@ -25,21 +25,21 @@ prices = np.array(closed, dtype='f8') # 浮動小数点数による NumPy 配列
 #date = np.array(stday, dtype='f8') #浮動小数点数による NumPy 配列にしておく
 
 # 5 日単純移動平均を求める
-sma5 = ta.SMA(prices, timeperiod=5)
+#sma5 = ta.SMA(prices, timeperiod=5)
 
-# RSI (14 日) を求める
+# IRS (14 日) を求める
 rsi14 = ta.RSI(prices, timeperiod=14)
 
 # MACD (先行 12 日移動平均、遅行 26 日移動平均、 9 日シグナル線) を求める
-macd, macdsignal, macdhist = ta.MACD(prices,fastperiod=12, slowperiod=26, signalperiod=9)
+#macd, macdsignal, macdhist = ta.MACD(prices,fastperiod=12, slowperiod=26, signalperiod=9)
 fig, ax = plt.subplots()
 ax.xaxis.set_major_formatter(mdates.DateFormatter('%m/%d\n%H:%M'))
 
-plt.plot(df.index, sma5)
+#plt.plot(df.index, sma5)
 plt.plot(df.index, rsi14)
-plt.plot(df.index, macd)
-plt.plot(df.index, macdsignal)
-plt.plot(df.index, macdhist)
+#plt.plot(df.index, macd)
+#plt.plot(df.index, macdsignal)
+#plt.plot(df.index, macdhist)
 
 
     # プロット表示(設定の反映)
